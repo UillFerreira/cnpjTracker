c4c8dda7f827d9af97be839b93d7c302a356888f
william-ferreira <william-ferreira@specchio.info>
2024-03-14 13:59:46 -0300

    Adicionei o log

--- sql/cnpj__serpro_cnpj_select.sql
+++ sql/cnpj__serpro_cnpj_select.sql
@@ -4,6 +4,8 @@ DECLARE
     v_json_ret json;
 BEGIN
     SELECT json_data::json INTO v_json_ret FROM cnpj__data AS d WHERE cnpj_uuid = cnpj__data;
+    INSERT INTO cnpj__serpro_log (contract_uuid, key_uuid, msg) VALUES ('2f3aa007-c341-4342-b826-08a8873209bf', v_chave_uuid, 'Nova consulta de CNPJ serpro');
+
     RETURN v_json_ret;
 END;
 $$ LANGUAGE plpgsql;
