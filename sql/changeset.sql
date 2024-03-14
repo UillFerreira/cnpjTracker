86e866806cd4b25294aec9f77d525f58b035b404
William Costa Ferreira <william.ferreira.x@gmail.com>
2024-03-14 17:53:35 +0000

    Sem pendencias no server

--- sql/changeset.sql
+++
@@ -1,6 +0,0 @@
-create table cnpj__data(cnpj_uuid uuid not null unique, updated timestamptz default now(), json_data jsonb not null, FOREIGN KEY (cnpj_uuid) REFERENCES cnpj__list(cnpj_uuid));
-create table cnpj__nfe_list(chave_uuid uuid not null default gen_random_uuid() primary key, chave varchar(44) unique not null, updated timestamptz default now(), json_data jsonb not null, sha1 text not null unique);
-drop table cnpj__serpro_log;
-create table cnpj__serpro_log (contract_uuid uuid not null references cnpj__api_serpro (contract_uuid), key_uuid uuid not null, msg text, created timestamptz not null default now());
-drop function cnpj__serpro_cnd_select ( character varying);
-select * from auth__grant_execute('login');
